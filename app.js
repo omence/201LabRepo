e398a26f636fc70576b1d94f52c80ffa55954795
xgingerbreadmanx <rjjohnson1204@gmail.com>
2018-10-25 14:51:21 -0700

    created function

--- app.js
+++ app.js
@@ -2,94 +2,84 @@
 
 var score = 0;
 var userNme = prompt('what is your name?');
+
 // inital instructions to user
 var initialAlert = alert(
   'PLEASE ANSWER QUESTIONS WITH (y, Y, yes, YES or n, N, no, NO )'
 );
 
-console.log('q1 should return undefined', quesOne);
+function question( varible1, varible2) {
+if ( varible1 === varible2 ){
+ alert( 'correct'); score++; } 
+else { alert ('wrong') }
+console.log(' ')
+return quesOneAns;
+}
 var quesOne = prompt('Was I born on mars?').toUpperCase();
+var quesOneAns = 'N'
+question( quesOne, quesOneAns);
 
-// questions and alert to responses
-if (quesOne === 'N' || quesOne === 'NO') {
-  alert('correct');
-  score++;
-} else {
-  alert('Wrong');
-}
 
-console.log('q2 should return undefined', quesTwo);
 var quesTwo = prompt('Am I just a tad bit over weight?').toUpperCase();
+var quesTwoAns = 'Y'
+question( quesTwo, quesTwoAns);
 
-if (quesTwo === 'Y' || quesTwo === 'YES') {
-  alert('correct');
-  score++;
-} else {
-  alert('Wrong');
-}
 
-console.log('q3 should return undefined', quesThree);
 var quesThree = prompt('Do I exist?').toUpperCase();
+var quesThreeAns = 'Y'
+question( quesThree, quesThreeAns);
 
-if (quesThree === 'Y' || quesThree === 'YES') {
-  alert('correct');
-  score++;
-} else {
-  alert('Wrong');
-}
+// var quesFour = prompt('DO I have an age?').toUpperCase();
 
-console.log('q4 should return undefined', quesFour);
-var quesFour = prompt('DO I have an age?').toUpperCase();
+// if (quesFour === 'Y' || quesFour === 'YES') {
+//   alert('correct');
+//   score++;
+// } else {
+//   alert('Wrong');
+// }
 
-if (quesFour === 'Y' || quesFour === 'YES') {
-  alert('correct');
-  score++;
-} else {
-  alert('Wrong');
-}
+// console.log('q1 should return undefined', quesFive);
+// var quesFive = prompt('Are my toes growing from my face?').toUpperCase();
 
-console.log('q1 should return undefined', quesFive);
-var quesFive = prompt('Are my toes growing from my face?').toUpperCase();
+// if (quesFive === 'N' || quesFive === 'NO') {
+//   alert('correct');
+//   score++;
+// } else {
+//   alert('Wrong');
+// }
 
-if (quesFive === 'N' || quesFive === 'NO') {
-  alert('correct');
-  score++;
-} else {
-  alert('Wrong');
-}
-
-var ansSix = '5';
-var quesSix = prompt('What is my favoriate number between one and ten?');
-if (quesSix === ansSix) {
-  alert('correct');
-  score++;
-} else {
-  var counter = 0;
-  while (counter < 3 && quesSix !== ansSix)
-    prompt('no try again');
-      counter++;
-    } if (quesSix === ansSix) {alert('correct');
-      score++;
-    }
+// var ansSix = '5';
+// var quesSix = prompt('What is my favoriate number between one and ten?');
+// if (quesSix === ansSix) {
+//   alert('correct');
+//   score++;
+// } else {
+//   var counter = 0;
+//   while (counter < 3 && quesSix !== ansSix)
+//     prompt('no try again');
+//       counter++;
+//     } if (quesSix === ansSix) {alert('correct');
+//       score++;
+//     }
   
 
 
-var turnsLeft = 6;
-var stop = true;
-while (turnsLeft > 0) {
-  var answers = ['seattle', 'fresno', 'visalia', 'hanford'];
-  var quesSeven = prompt('Guess one of 4 cities I have lived in.');
-  for (var i = 0; i < answers.length; i++) {
-    if (answers[i] === quesSeven && stop === true) {
-      alert('correct');
-      score++;
-      break;
-      stop = false;
-    } else {
-      quesSeven = prompt('wrong try again');
-      turnsLeft--;
-    }
-  }
-}
+// var turnsLeft = 6;
+// var stop = true;
+// while (turnsLeft > 0) {
+//   var answers = ['seattle', 'fresno', 'visalia', 'hanford'];
+//   var quesSeven = prompt('Guess one of 4 cities I have lived in.');
+//   for (var i = 0; i < answers.length; i++) {
+//     if (answers[i] === quesSeven && stop === true) {
+//       alert('correct');
+//       score++;
+//       break;
+//       stop = false;
+//     } else {
+//       quesSeven = prompt('wrong try again');
+//       turnsLeft--;
+//     }
+//   }
+// }
 
-var scoreDisp = alert('You scored' + score + 'good job' + userNme);
+// var scoreDisp = alert('You scored' + score + 'good job' + userNme);
