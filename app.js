1b4f5fbe0fdec9563836b476ed51677c1d9c7ff7
jason few <omence11@gmail.com>
2018-10-27 09:03:36 -0700

    lab4

--- app.js
+++ app.js
@@ -4,82 +4,84 @@ var score = 0;
 var userNme = prompt('what is your name?');
 
 // inital instructions to user
-var initialAlert = alert(
-  'PLEASE ANSWER QUESTIONS WITH (y, Y, yes, YES or n, N, no, NO )'
-);
+var initialAlert = alert('PLEASE ANSWER QUESTIONS WITH (YES or NO )');
 
-function question( varible1, varible2) {
-if ( varible1 === varible2 ){
- alert( 'correct'); score++; } 
-else { alert ('wrong') }
-console.log(' ')
-return quesOneAns;
+function question(varible1, varible2) {
+  if (varible1 === varible2) {
+    alert('correct');
+    score++;
+  } else {
+    alert('wrong');
+  }
+  console.log(' ');
+  return quesOneAns;
 }
 var quesOne = prompt('Was I born on mars?').toUpperCase();
-var quesOneAns = 'N'
-question( quesOne, quesOneAns);
-
+var quesOneAns = 'NO';
+question(quesOne, quesOneAns);
 
 var quesTwo = prompt('Am I just a tad bit over weight?').toUpperCase();
-var quesTwoAns = 'Y'
-question( quesTwo, quesTwoAns);
-
+var quesTwoAns = 'YES';
+question(quesTwo, quesTwoAns);
 
 var quesThree = prompt('Do I exist?').toUpperCase();
-var quesThreeAns = 'Y'
-question( quesThree, quesThreeAns);
+var quesThreeAns = 'YES';
+question(quesThree, quesThreeAns);
 
-// var quesFour = prompt('DO I have an age?').toUpperCase();
+var quesFour = prompt('DO I have an age?').toUpperCase();
+var quesFourAns = 'YES';
+question(quesFour, quesFourAns);
 
-// if (quesFour === 'Y' || quesFour === 'YES') {
-//   alert('correct');
-//   score++;
-// } else {
-//   alert('Wrong');
-// }
+var quesFive = prompt('Are my toes growing from my face?').toUpperCase();
+var quesFiveAns = 'NO';
+question(quesFive, quesFiveAns);
 
-// console.log('q1 should return undefined', quesFive);
-// var quesFive = prompt('Are my toes growing from my face?').toUpperCase();
+function guessNumberQuestion() {
+  var ansSix = '5';
+  var quesSix = prompt('What is my favoriate number between one and ten?');
+  if (quesSix === ansSix) {
+    alert('correct');
+    score++;
+  } else if (quesSix !== ansSix) {
+    var counter = 0;
+    while (counter < 3 && quesSix !== ansSix) {
+      quesSix = prompt('no try again');
+      counter++;
+    }
+    if (quesSix === ansSix) {
+      alert('correct');
+      score++;
+    }
+  }
+}
 
-// if (quesFive === 'N' || quesFive === 'NO') {
-//   alert('correct');
-//   score++;
-// } else {
-//   alert('Wrong');
-// }
+guessNumberQuestion();
 
-// var ansSix = '5';
-// var quesSix = prompt('What is my favoriate number between one and ten?');
-// if (quesSix === ansSix) {
-//   alert('correct');
-//   score++;
-// } else {
-//   var counter = 0;
-//   while (counter < 3 && quesSix !== ansSix)
-//     prompt('no try again');
-//       counter++;
-//     } if (quesSix === ansSix) {alert('correct');
-//       score++;
-//     }
-  
+function guessCityQuestion() {
+  var answers = ['SEATTLE', 'FRESNO', 'VISALIA', 'HANFORD'];
+  var quesSeven = prompt(
+    'Guess one of 4 cities I have lived in.'
+  ).toUpperCase();
+  var answer = false;
+  var turnCount = 5;
 
+  while (turnCount && answer === false) {
+    for (var i = 0; i < answers.length; i++) {
+      if (answers[i] === quesSeven) {
+        answer = true;
+      }
+      if (answer) {
+        alert('correct');
+        score++;
+        break;
+      } else {
+        quesSeven = prompt('No please try again').toUpperCase();
+        turnCount--;
+      }
+    }
+  }
+}
 
-// var turnsLeft = 6;
-// var stop = true;
-// while (turnsLeft > 0) {
-//   var answers = ['seattle', 'fresno', 'visalia', 'hanford'];
-//   var quesSeven = prompt('Guess one of 4 cities I have lived in.');
-//   for (var i = 0; i < answers.length; i++) {
-//     if (answers[i] === quesSeven && stop === true) {
-//       alert('correct');
-//       score++;
-//       break;
-//       stop = false;
-//     } else {
-//       quesSeven = prompt('wrong try again');
-//       turnsLeft--;
-//     }
-//   }
-// }
+guessCityQuestion();
 
-// var scoreDisp = alert('You scored' + score + 'good job' + userNme);
+var scoreDisp = alert('You scored ' + score + ' good job ' + userNme);
